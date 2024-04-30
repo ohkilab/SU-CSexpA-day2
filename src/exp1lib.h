3f9845179a0c1cef945bd72f19fbacefbdd34ab2
joko0811 <perry_0096@docomo.ne.jp>
2024-04-30 19:04:03 +0900

    fix lang format

--- src/exp1lib.h
+++ src/exp1lib.h
@@ -1,6 +1,6 @@
 
 /**
- * prototype 
+ * prototype
  */
 int exp1_tcp_listen(int port);
 int exp1_tcp_connect(const char *hostname, int port);
@@ -8,4 +8,3 @@ int exp1_udp_listen(int port);
 int exp1_udp_connect(const char *hostname, int port);
 double gettimeofday_sec();
 int exp1_do_talk(int sock);
-
